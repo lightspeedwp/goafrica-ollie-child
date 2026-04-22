55e0b6e25837809ec25c00fde63262b814594d50
Zared Rogers <zared@lightspeedwp.agency>
2026-04-22 12:31:03 +0200

    Updateing Travel Style card

---
+++ patterns/ga-travel-style-card.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Title: Travel Style Card
+ * Slug: go-africa-ollie-child/ga-travel-style-card
+ * Inserter: yes
+ */
+?>
+<!-- wp:cover {"url":"/wp-content/uploads/2026/04/Onze-Safari-Auto.webp","id":1448,"isUserOverlayColor":true,"customGradient":"linear-gradient(180deg,rgba(0,0,0,0) 30%,rgb(0,0,0) 70%)","contentPosition":"bottom center","sizeSlug":"full","className":"is-style-ga-travel-style-card","style":{"border":{"radius":{"topLeft":"var:preset|border-radius|sm","topRight":"var:preset|border-radius|sm","bottomLeft":"var:preset|border-radius|sm","bottomRight":"var:preset|border-radius|sm"}},"spacing":{"blockGap":"0"},"dimensions":{"aspectRatio":"1/1"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover has-custom-content-position is-position-bottom-center is-style-ga-travel-style-card" style="border-top-left-radius:var(--wp--preset--border-radius--sm);border-top-right-radius:var(--wp--preset--border-radius--sm);border-bottom-left-radius:var(--wp--preset--border-radius--sm);border-bottom-right-radius:var(--wp--preset--border-radius--sm)"><img class="wp-block-cover__image-background wp-image-1448 size-full" alt="" src="/wp-content/uploads/2026/04/Onze-Safari-Auto.webp" data-object-fit="cover"/><span aria-hidden="true" class="wp-block-cover__background has-background-dim-100 has-background-dim wp-block-cover__gradient-background has-background-gradient" style="background:linear-gradient(180deg,rgba(0,0,0,0) 30%,rgb(0,0,0) 70%)"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"level":3,"style":{"typography":{"textAlign":"center","fontStyle":"normal","fontWeight":"500"},"border":{"radius":{"topLeft":"0","topRight":"0","bottomLeft":"0","bottomRight":"0"}}},"textColor":"base","fontSize":"medium"} -->
+<h3 class="wp-block-heading has-text-align-center has-base-color has-text-color has-medium-font-size" style="border-top-left-radius:0;border-top-right-radius:0;border-bottom-left-radius:0;border-bottom-right-radius:0;font-style:normal;font-weight:500">Safari</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"className":"hidden-excerpt","style":{"typography":{"textAlign":"center","fontStyle":"normal","fontWeight":"300"}},"textColor":"base","fontSize":"small"} -->
+<p class="has-text-align-center hidden-excerpt has-base-color has-text-color has-small-font-size" style="font-style:normal;font-weight:300"><strong>Ook zo gek op de wilde dieren van Afrika? Hier vind je reizen waar je de Big Five kunt spotten.</strong></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover -->
